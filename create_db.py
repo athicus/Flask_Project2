e421ab8ad33b7be434119a26c59b550185691494
athicus <alexanb7@gmail.com>
2023-04-28 20:57:13 -0400

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,5 +1,5 @@
 import sqlite3 as sql
-conn = sqlite3.connect('database.db')
+conn = sql.connect('database.db')
 print("Opened	database	successfully")
 conn.execute('CREATE TABLE students (EmpID Text, EmpName	TEXT,	EmpGender	TEXT,	EmpPhone TEXT,	EmpBdate	TEXT)')
 print("Table created	successfully")
