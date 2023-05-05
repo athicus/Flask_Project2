3f27ece8070ddf3c5192cb06574574afccb5bc49
athicus <alexanb7@gmail.com>
2023-05-05 18:56:43 -0400

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -1,11 +1,11 @@
 import mysql.connector as sql
 conn = sql.connect(host="localhost",	user="flask",	password="ubuntu",	database="flask_db")
 cur = conn.cursor()
-cmd = "CREATE	TABLE	employees	(\
-				EmpID	INT	NOT	NULL	AUTO_INCREMENT	PRIMARY	KEY,	\
-				EmpName	VARCHAR(30)	NOT	NULL,\
-				EmpGender	CHAR(1),	\
-				EmpPhone VARCHAR(15),	\
-				EmpBdate	VARCHAR(30))"
+cmd = "CREATE TABLE employees (\
+	EmpID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+	EmpName	VARCHAR(30) NOT	NULL, \
+	EmpGender CHAR(1), \
+	EmpPhone VARCHAR(15), \
+	EmpBdate VARCHAR(30))"
 cur.execute(cmd)
 conn.close()
