cce2b37ddb6cbf860b422006430e77ccfd68c143
athicus <alexanb7@gmail.com>
2023-05-15 20:28:02 -0400

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -2,7 +2,7 @@ import mysql.connector as sql
 conn = sql.connect(host="localhost",	user="flask",	password="ubuntu",	database="flask_db")
 cur = conn.cursor()
 cmd = "CREATE TABLE appointments (\
-	LicPlate INT NOT NULL PRIMARY KEY,\
+	LicPlate VARCHAR(15) NOT NULL PRIMARY KEY,\
 	CusName	VARCHAR(30) NOT	NULL, \
 	CarType VARCHAR(15), \
 	CusPhone VARCHAR(15), \
